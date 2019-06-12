392dc9322717bf410799beea5072462e7e1149bd
hanyan123456 <3190922875@qq.com>
2019-06-12 20:47:56 +0800

    uptate

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,21 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 class Home extends Component {
-    state ={
-        username:''
+    state = {
+        username: '',
+        toggleShow: true
     }
-    inputChange= (e) =>{
+    inputChange = (e) => {
         console.log(e.target.value)
         this.setState({
-            username:e.target.value
+            username: e.target.value
         })
     }
+    toggleData = () => {
+        this.setState({ toggleShow: !this.state.toggleShow });
+    }
     render() {
-        const {username} =this.state
+        const { username, toggleShow } = this.state
         return (
             <div>
-                <label>name</label><input type="text" placeholder="请输入" onChange={(e)=>this.inputChange(e)}/>
-                <span>{username}</span>
+                <label>name</label><input type="text" placeholder="请输入" onChange={(e) => this.inputChange(e)} />
+                {toggleShow && <span>{username} </span>}
+                <button onClick={this.toggleData}>submit</button>
             </div>
         );
     }
